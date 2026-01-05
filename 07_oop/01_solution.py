afc45b3fc6abbc050d71f393e59e2f3f89a1e527
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2026-01-05 15:00:29 +0500

    encapsulation

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -1,21 +1,45 @@
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+
+#     def get_brand(self):
+#         return self.brand + " !"
+        
+#     def full_name(self):
+#         return f"{self.brand} {self.model}"            
+
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+# my_tesla = ElectricCar("Tesla", "Model S", "100KWH")
+
+# print(my_tesla.get_brand())
+# print(my_tesla.full_name())
+
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+
+# my_car = Car("Toyota", "Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+
+# my_new_car = Car("Honda", "Civic")
+# print(my_new_car.brand)
+# print(my_new_car.model)
+
+
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
 
-my_car = Car("Toyota", "Corolla")
-print(my_car.brand)
-print(my_car.model)
-
-my_new_car = Car("Honda", "Civic")
-print(my_new_car.brand)
-print(my_new_car.model)
-
-
-class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
+    def get_brand(self):
+        return self.brand + " !"
 
     def full_name(self):
         return f"{self.brand} {self.model}"
@@ -34,3 +58,4 @@ print(my_tesla.brand)
 print(my_tesla.model)
 print(my_tesla.battery_size)
 print(my_tesla.full_name())
+print(my_tesla.get_brand())
