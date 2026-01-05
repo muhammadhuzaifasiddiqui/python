fca1b36368bce218bf36e742fdcf821015320326
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2026-01-05 16:10:24 +0500

    Add total_car attribute and fuel_type methods to Car and ElectricCar classes

--- 07_oop/01_solution.py
+++ 07_oop/01_solution.py
@@ -34,18 +34,24 @@
 
 
 class Car:
+    total_car = 0
+
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+        Car.total_car += 1
 
     def get_brand(self):
         return self.brand + " !"
 
     def full_name(self):
         return f"{self.brand} {self.model}"
-    
-my_car = Car("Toyota", "Corolla")
-print(my_car.full_name())
+
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
+# my_car = Car("Toyota", "Corolla")
+# print(my_car.full_name())
 
 
 class ElectricCar(Car):
@@ -53,9 +59,23 @@ class ElectricCar(Car):
         super().__init__(brand, model)
         self.battery_size = battery_size
 
+    def fuel_type(self):
+        return "Electric Charge"
+
 my_tesla = ElectricCar("Tesla", "Model S", "100KWH")
-print(my_tesla.brand)
-print(my_tesla.model)
-print(my_tesla.battery_size)
-print(my_tesla.full_name())
-print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
+
+safari = Car("Tata", "Safari")
+safariThree = Car("Tata", "Nexon")
+print(safari.fuel_type())
+# print(safari.total_car)
+# test = Car("test", "test")
+# print(test.total_car)
+print(Car.total_car)
+
+
+# print(my_tesla.brand)
+# print(my_tesla.model)
+# print(my_tesla.battery_size)
+# print(my_tesla.full_name())
+# print(my_tesla.get_brand())
