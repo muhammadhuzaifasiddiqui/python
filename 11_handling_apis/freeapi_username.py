05824cd6fc01c0bb62f9e50c6063259539f9e495
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2026-01-09 11:52:10 +0500

    relocate freeapi_username file

--- 11_handling_apis/freeapi_username.py
+++ 11_handling_apis/freeapi_username.py
@@ -12,7 +12,7 @@ def fetch_random_user_freeapi():
         return username, country
     else:
         raise Exception("Failed to fetch user data")
-    
+
 def main():
     try:
         username, country = fetch_random_user_freeapi()
@@ -24,4 +24,3 @@ if __name__ == "__main__":
     main()
 
 
-    
